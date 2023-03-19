94e7fffc33ac00acaa72c7d50a852ffbcc68e4f7
Doug Feldmann <douglas_feldmann@hms.harvard.edu>
2023-03-19 15:17:46 -0400

    documentation

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+set -e
+
+# usage:  install.sh [/path/to/desired/bin]
+# if path is not specified, defaults to $HOME/bin
+#
+# This script will install pkgwhat.clj, create a trivial
+# bash wrapper named pkgwhat, with commands symlinked to
+# the wrapper.
+
 scriptdir=$(dirname $(realpath $0))
 src_script="$scriptdir/pkgwhat.clj"
 if test -z "$1"
@@ -8,10 +17,19 @@ else
     targetbin=$1
 fi
 targ_pkgwhat=$targetbin/pkgwhat
+dst_script=$targetbin/pkgwhat.clj
+
+if ! test -d $targetbin
+then
+    echo $targetbin does not exist
+    exit 1
+fi
+
+install -m 755 $src_script $dst_script
 
 cat <<EOF > $targ_pkgwhat
 #!/usr/bin/env bash
-$src_script \$(basename \$0) \$@
+$dst_script \$(basename \$0) \$@
 EOF
 
 chmod 755 $targ_pkgwhat
